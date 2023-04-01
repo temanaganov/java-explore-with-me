d7e9113eef88235e5f295dcfa1b1bff34be0a007
Artem Naganov <artemnaganov10@gmail.com>
2023-04-01 23:29:57 +0300

    fix: правки

--- stats/stats-client/src/main/java/ru/practicum/stats/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/stats/client/StatsClient.java
@@ -3,7 +3,9 @@ package ru.practicum.stats.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -27,7 +29,16 @@ public class StatsClient {
         webClient.post()
                 .uri("/hit")
                 .body(Mono.just(createEndpointHitDto), CreateEndpointHitDto.class)
-                .retrieve();
+                .exchangeToMono(response -> {
+                    if (response.statusCode().equals(HttpStatus.CREATED)) {
+                        return response.bodyToMono(Object.class)
+                                .map(body -> ResponseEntity.status(HttpStatus.CREATED).body(body));
+                    } else {
+                        return response.createException()
+                                .flatMap(Mono::error);
+                    }
+                })
+                .block();
     }
 
     public Mono<List<ViewStats>> getStatistics() {
