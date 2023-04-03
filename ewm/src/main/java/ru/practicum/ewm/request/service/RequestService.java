90abc1ce474fe6ad9ce1b992acb539c21aaa4112
Artem Naganov <artemnaganov10@gmail.com>
2023-04-04 00:06:02 +0300

    fix: checkstyle

--- ewm/src/main/java/ru/practicum/ewm/request/service/RequestService.java
+++ ewm/src/main/java/ru/practicum/ewm/request/service/RequestService.java
@@ -18,7 +18,6 @@ import ru.practicum.ewm.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
